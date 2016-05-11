f7d5605372cbcae12e0b34dda63a81d18824a2f0
Estudiante <Estudiante@10.203.3.56>
2016-05-11 15:24:34 -0500

    version 1.0.0

--- src/drawing/Tablero.java
+++ src/drawing/Tablero.java
@@ -10,6 +10,8 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 /**
  *
  * @author Estudiante
@@ -24,6 +26,32 @@ public class Tablero extends JPanel implements ActionListener, KeyListener{
       this.personajePrincipal= new Carro(100,200);
       this.timer = new Timer(20,this);
       this.timer.start();
+    }
+        @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+      this.personajePrincipal.paintComponent(g);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      this.personajePrincipal.actionPerformed(e);
+      repaint();
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        this.personajePrincipal.keyPressed(e);
+        repaint();
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+      this.personajePrincipal.keyReleased(e);
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+      this.personajePrincipal.keyTyped(e);
+        
     }
     
 }
